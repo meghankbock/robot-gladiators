9265bd56325c0cf2aafaede8def6604b2bad741d
Meghan Bock <meghan.bock@gmail.com>
2021-05-25 02:39:30 -0500

    high score update

--- assets/js/game.js
+++ assets/js/game.js
@@ -205,8 +205,27 @@ var startGame = function () {
 };
 
 var endGame = function () {
+  window.alert(" The game has now ended. Let's see how you did");
+
+  // check localStorage for high score, if it's not there use 0
+  var highScore = localStorage.getItem("highscore");
+  if (highScore === null) {
+      highscore = 0;
+  }
+
+  // if player has more money than the high score, player has a new high score
+  if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
+
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+  }
+  else {
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+  }
+
   // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
+  /*if (playerInfo.health > 0) {
     window.alert(
       "Great job, you've survived the game! You now have a score of " +
         playerInfo.money +
@@ -214,8 +233,9 @@ var endGame = function () {
     );
   } else {
     window.alert("You've lost yoru robot in battle.");
-  }
-
+  }*/
+  
+  // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
 
   if (playAgainConfirm) {
